1a02c463d4d5bce4db927617470deb1c9d8effaf
Piotr Styczyński <piotr@styczynski.in>
2021-11-17 22:43:44 +0100

    Cleanup go_sat_solver and integrate the sources

--- go_sat_solver/go_sat_solver/web/go-sat-solver-web/src/components/SolverSpinner/solverSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
-
-export const SolverSpinner = () => {
-    return (
-        <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
-    );
-};
