d9a9de7545f695b4795e19d0b0f92fec3eeaf3e7
Piotr Styczyński <piotr@styczynski.in>
2021-11-18 15:58:13 +0100

    Improve tests

--- cmd/latte-compiler/main.go
+++ cmd/latte-compiler/main.go
@@ -3,13 +3,8 @@ package main
 import (
 	_ "github.com/styczynski/latte-compiler/src/compiler/jvm"
 	"github.com/styczynski/latte-compiler/src/compiler_pipeline"
-	sat_solver "github.com/styczynski/latte-compiler/src/sat_solver/core"
 )
 
-func mainx() {
-	(compiler_pipeline.RunCompilerPipelineCliInterface{}).Run()
-}
-
 func main() {
-	sat_solver.TestComputeGraphColouring()
+	(compiler_pipeline.RunCompilerPipelineCliInterface{}).Run()
 }
