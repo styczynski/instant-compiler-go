163edb5ece2b99c6728afdfefbd52acc84fc9342
Piotr Styczyński <piotr@styczynski.in>
2021-11-30 21:33:21 +0100

    Release pipeline

--- pack.sh
+++ pack.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 DIR=$(pwd)
-rm -rfd *.tar.gz
-cd ..
-cp -R $DIR $DIR/ps386038
-cd $DIR
-tar -czvf ps386038.tar.gz ./ps386038
-rm -rfd ./ps386038
-mv ../ps386038.tar.gz .
+rm -rfd *.tar.gz && \
+cd .. && \
+cp -R $DIR /tmp/ps386038 && \
+cd $DIR && \
+rm -rfd /tmp/ps386038/.git && \
+rm -rfd /tmp/ps386038/.go && \
+rm -rfd /tmp/ps386038/tests && \
+cd /tmp/ && \
+tar -czvf ps386038.tar.gz ./ps386038 && \
+cd $DIR && \
+rm -rfd ./ps386038 && \
+mv /tmp/ps386038.tar.gz .
